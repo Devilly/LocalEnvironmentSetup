679d6ae73d301e36de73c8d3a5b776a6fc49f6f8
Dennis van Oosterhout <de.slotenzwemmer@gmail.com>
2021-02-11 20:05:20 +0100

    Adapt to new file name

--- windows.ps1
+++ windows.ps1
@@ -2,19 +2,19 @@
 Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
 
 # Set explorer settings
-$explorerAdvancedSettingsPath = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+$path = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
 
-Set-ItemProperty -Path $explorerAdvancedSettingsPath -Name HideFileExt -Value 0
-Set-ItemProperty -Path $explorerAdvancedSettingsPath -Name Hidden -Value 1
+Set-ItemProperty -Path $path -Name HideFileExt -Value 0
+Set-ItemProperty -Path $path -Name Hidden -Value 1
 
 # Install winget
-$localFile = "./winget.appxbundle"
+$path = Join-Path . winget.appxbundle
 
-Start-BitsTransfer -Source https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle -Destination $localFile
+Start-BitsTransfer -Source https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle -Destination $path
 
-Add-AppxPackage -Path $localFile
+Add-AppxPackage -Path $path
 
-Remove-Item -Path $localFile
+Remove-Item -Path $path
 
 # Install other software
 $applications = "Google.Chrome", "Microsoft.VisualStudioCode-User-x64", "Microsoft.WindowsTerminal", "Microsoft.PowerShell", "Docker.DockerDesktop", "SlackTechnologies.Slack", "Microsoft.Teams"
@@ -24,7 +24,8 @@ foreach($application in $applications) {
 }
 
 # Install VolumeCtrl
-Start-BitsTransfer -Source https://github.com/Devilly/VolumeCtrl/releases/latest/download/VolumeControl.exe -Destination "$([Environment]::GetFolderPath([Environment+SpecialFolder]::Startup))/VolumeCtrl.exe"
+$path = Join-Path ([Environment]::GetFolderPath([Environment+SpecialFolder]::Startup)) VolumeCtrl.exe
+Start-BitsTransfer -Source https://github.com/Devilly/VolumeCtrl/releases/latest/download/VolumeCtrl.exe -Destination $path
 
 # Restart
 Restart-Computer -Confirm
